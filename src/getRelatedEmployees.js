72cf94b932e3e53384a182144bf22d89ebdb6daf
Henrique Vilela <hv_santos@outlook.com>
2022-09-29 16:48:05 -0300

    Req 4 done

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const filteredManagers = data.employees.map(({ managers }) => managers);
+  return filteredManagers.some((eleme) => eleme.some((a) => a === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const someFunc = ({ managers }) => managers.some((idMan) => idMan === managerId);
+  const filteredManagers = data.employees.filter(someFunc);
+  if (filteredManagers.length === 0) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return filteredManagers.map((obj) => `${obj.firstName} ${obj.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
