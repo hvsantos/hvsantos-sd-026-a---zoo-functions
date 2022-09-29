1e2624ba79be178bed976c0bfa17962e19970ff2
Henrique Vilela <hv_santos@outlook.com>
2022-09-29 17:59:16 -0300

    Req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === '' || animal === undefined) {
+    const animalObj = {};
+    data.species.forEach(({ name, residents }) => { animalObj[name] = residents.length; });
+    return animalObj;
+  }
+  const { specie, sex = '' } = animal;
+  let selectedAnimal = data.species.find((eleme) => eleme.name === specie);
+  const reduceFunc = (a, b) => ((b.sex === sex || sex === '') ? a + 1 : a);
+  selectedAnimal = selectedAnimal.residents.reduce(reduceFunc, 0);
+  return selectedAnimal;
 }
 
 module.exports = countAnimals;
