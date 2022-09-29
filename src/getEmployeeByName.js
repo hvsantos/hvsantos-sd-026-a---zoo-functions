964497159843f86f7f31c831a633f1e23808858f
Henrique Vilela <hv_santos@outlook.com>
2022-09-29 15:51:51 -0300

    Req 3 done

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === '' || employeeName === undefined) {
+    return {};
+  }
+  const employee = employeeName;
+  const filterFunc = ({ firstName, lastName }) => firstName === employee || lastName === employee;
+  const [retorno] = data.employees.filter(filterFunc);
+  return retorno;
 }
 
 module.exports = getEmployeeByName;
