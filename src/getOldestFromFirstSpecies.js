243b7d52742e0d0880918333674ad5083f50db2c
Henrique Vilela <hv_santos@outlook.com>
2022-09-29 21:12:50 -0300

    Req 10 Done

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find(({ id: idEmployee }) => idEmployee === id);
+  const firstAni = data.species.find(({ id: idAni }) => idAni === employee.responsibleFor[0]);
+  const selected = firstAni.residents.sort(({ age: aAge }, { age: bAge }) => bAge - aAge)[0];
+  return [selected.name, selected.sex, selected.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
