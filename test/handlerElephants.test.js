45a2845a75213081e5c94ba6c9d3e96df13841b7
Henrique Vilela <hv_santos@outlook.com>
2022-09-30 12:07:42 -0300

    Req 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,19 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se a função existe', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Verifica se só aceita string', () => {
+    expect(handlerElephants([1, 2])).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Verifica se retorna 4 ao inserir count', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Verifica se retorna um array com Jefferson ao inserir names', () => {
+    expect(handlerElephants('names').includes('Jefferson')).toBe(true);
+  });
+  it('Verifica se retorna um número proximo de 10.5 ao inserir averageAge', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+});
